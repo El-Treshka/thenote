ba4087eedba97eaa47bea01725c591984a827003
El-Treshka <msogil847@gmail.com>
2020-12-08 18:01:01 +0200

    added security support

--- src/main/java/ua/trshk/note/bean/SpringFoxConfig.java
+++ src/main/java/ua/trshk/note/bean/SpringFoxConfig.java
@@ -3,9 +3,14 @@ package ua.trshk.note.bean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.*;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Collections;
+import java.util.List;
 
 import static springfox.documentation.builders.PathSelectors.regex;
 
@@ -14,10 +19,39 @@ import static springfox.documentation.builders.PathSelectors.regex;
 public class SpringFoxConfig {
     @Bean
     public Docket api() {
-        return new Docket(DocumentationType.SWAGGER_2).select()
+        return new Docket(DocumentationType.SWAGGER_2).apiInfo(apiInfo())
+                .securityContexts(Collections.singletonList(securityContext()))
+                .securitySchemes(Collections.singletonList(apiKey())).select()
                 .apis(RequestHandlerSelectors.basePackage("ua.trshk.note.controller"))
                 .paths(regex("/api.*")).build();
 
     }
+
+    private ApiKey apiKey() {
+        return new ApiKey("JWT", "Authorization", "header");
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfo(
+                "The Notes REST API",
+                "API",
+                "1.0",
+                "Terms of service",
+                new Contact("Miroslav", "", ""),
+                "License of API",
+                "API license URL",
+                Collections.emptyList());
+    }
+    private SecurityContext securityContext() {
+        return SecurityContext.builder().securityReferences(defaultAuth()).build();
+    }
+
+    private List<SecurityReference> defaultAuth() {
+        AuthorizationScope authorizationScope = new AuthorizationScope("global", "accessEverything");
+        AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
+        authorizationScopes[0] = authorizationScope;
+        return Collections.singletonList(new SecurityReference("JWT", authorizationScopes));
+    }
+
 }
 
